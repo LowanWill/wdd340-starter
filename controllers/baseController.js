0b4ddd584221d43429a60c9d42eccdabff4196ce
LowanWill <lowannwil@byui.edu>
2025-05-31 09:58:14 -0600

    updates to css and some corrections

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,7 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  //req.flash("notice", "This is a flash message.")    -- this is how you add a flash message from a controller to a view
   res.render("index", {title: "Home", nav})
 }
 
